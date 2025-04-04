8e27843f158e495b93d1e979a6ab6b6f949b61ac
Jean Escobar <jeanescobar7@hotmail.com>
2025-04-04 01:22:59 -0500

    no se va a utilizar el input password

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,7 +1,5 @@
-import { IconEyeClose, IconEyeOpen } from '@/assets/icons/IconEye';
 import { IconCalendar } from '@/assets/icons/IconCalendar';
 import IconWarning from '@/assets/icons/IconWarning';
-import { useState } from 'react';
 import './input.scss';
 interface InputProps {
     value: string;
@@ -9,7 +7,6 @@ interface InputProps {
     props?: React.InputHTMLAttributes<HTMLInputElement | HTMLTextAreaElement>;
     error?: string;
     touch?: boolean;
-    password?: boolean;
     placeholder?: string;
     disabled?: boolean;
     showWarning?: boolean;
@@ -23,14 +20,12 @@ export const Input = ({
     props,
     error,
     touch,
-    password,
     placeholder,
     disabled = false,
     showWarning = false,
     showErrorMessage = false,
     type = 'text',
 }: InputProps) => {
-    const [showPassword, setShowPassword] = useState(false);
 
     const formatDate = (input: string) => {
         const digits = input.replace(/\D/g, '');
@@ -104,26 +99,8 @@ export const Input = ({
                         autoComplete="off"
                         value={value}
                         onChange={handleTextChange}
-                        type={
-                            password
-                                ? showPassword
-                                    ? 'text'
-                                    : 'password'
-                                : type
-                        }
+                        type={type}
                     />
-                )}
-                {password && (
-                    <picture
-                        onClick={() => setShowPassword(!showPassword)}
-                        style={{
-                            cursor: 'pointer',
-                            position: 'absolute',
-                            right: '15px',
-                        }}
-                    >
-                        {showPassword ? <IconEyeClose /> : <IconEyeOpen />}
-                    </picture>
                 )}
                 {showWarning && error && touch && (
                     <div

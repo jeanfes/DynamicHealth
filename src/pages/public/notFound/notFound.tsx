8d1f36475be142b6d48d3da01eb57f7333d52f4f
jeanfes <jescobar@solucionesamericana.com.co>
2025-04-04 12:25:09 -0500

    se agrego pagina de not found

--- src/pages/public/notFound/notFound.tsx
+++ src/pages/public/notFound/notFound.tsx
@@ -1,8 +1,15 @@
+import { useNavigate } from "react-router-dom";
+import "./notFound.scss";
+import { Button } from "@/components/button/Button";
+
 const NotFound = () => {
+    const navigate = useNavigate();
     return (
-        <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-4xl font-bold">404 - Not Found</h1>
-            <p className="mt-4 text-lg">The page you are looking for does not exist.</p>
+        <div className="notFoundContainer">
+            <h1>404</h1>
+            <h2>Página no encontrada</h2>
+            <p>Lo sentimos, la página que buscas no existe.</p>
+            <Button text="Regresar al inicio" onClick={() => navigate("/")} />
         </div>
     );
 }
